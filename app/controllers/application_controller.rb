c1cbcd38667fa60ed1ace6f78570bd8bacabcc6c
Mike Thai <mta24@sfu.ca>
2019-06-30 14:46:01 -0700

    add login, signup buttons, search link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def search
+        @articles = Article.where("title LIKE ?", "%#params[:search]%")   # ? replaces the place holder with the search arguments
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
- 
+  get 'search', to: "articles#search"   # get 'url' to "controlller_name#name_of_action"
+  
+  resources :welcome
   root 'welcome#index'
 end
