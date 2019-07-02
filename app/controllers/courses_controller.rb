9ab86bfa5f70db8bb63c42d242de510317e73831
Mike Thai <mta24@sfu.ca>
2019-07-02 01:09:16 -0700

    setting up course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,22 @@ class CoursesController < ApplicationController
         @courses = Course.search(params[:search])
     end
 
-    # # Download courses
-    # def download
-    # end
+    def show
+        @course = Course.find(params[:id])
+    end
 
+    def new
+    end
+
+    def create
+        @course = Course.new(course_params)
+
+        @course.save
+        redirect_to @course
+    end
+
+    private
+    def course_params
+        params.require(:course).permit(:subject, :number, :credits, :prerequisites, :corequisites)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'courses/index'
+  get 'courses/new'
   # get 'search', to: "welcome#search"   # get 'url' to "controller_name#name_of_action"
 
   resources :welcome
