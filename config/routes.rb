b1df09cc9facfc5fbfde4d7e68695b2287f29884
Mike Thai <mta24@sfu.ca>
2019-07-02 02:03:41 -0700

    updated courses MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'courses/index'
-  get 'courses/new'
+  # get 'courses/index'
+  # get 'courses/new'
   # get 'search', to: "welcome#search"   # get 'url' to "controller_name#name_of_action"
 
   resources :welcome
--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,13 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "courses", force: :cascade do |t|
+    t.string "subject"
+    t.integer "number"
+    t.integer "credits"
+    t.text "prerequisites"
+    t.text "corequisites"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
