dcc0c96bbe48874a4be505be0ab69eb7901e61fd
Mike Thai <mta24@sfu.ca>
2019-07-02 10:07:35 -0700

    bug fixes

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   # params :subject, :number, :credits, :prerequisites, :corequisites
   def self.search(search)
     if search
-      where(["cast(subject as string) LIKE ? or cast(number as string) LIKE ?", "%#{search}%", "%#{search}%"])
+      where(["cast(subject as text) LIKE ? or cast(number as text) LIKE ?", "%#{search}%", "%#{search}%"])
     else
       all
     end
