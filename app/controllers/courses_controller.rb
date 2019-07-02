13b8a20853e751d6a4d2f6b60f5a76818f423320
Mike Thai <mta24@sfu.ca>
2019-07-02 03:11:30 -0700

    add search functionality to courses page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
     def index
-        @courses = Course.all
+        # @courses = Course.all
+        @courses = Course.search(params[:search])
     end
 
     def show
