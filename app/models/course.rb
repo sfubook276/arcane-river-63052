e001417e3dbf1b9dea6d615eccd7e3a0dd630c46
Mike Thai <mta24@sfu.ca>
2019-07-04 13:43:38 -0700

    lowercase search

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   # params :subject, :number, :credits, :prerequisites, :corequisites
   def self.search(search)
     if search
-      where(["cast(subject as text) LIKE ? or cast(number as text) LIKE ?", "%#{search}%", "%#{search}%"])
+      where(["cast(subject as text) ILIKE ? or cast(number as text) ILIKE ?", "%#{search}%", "%#{search}%"])
     else
       all
     end
