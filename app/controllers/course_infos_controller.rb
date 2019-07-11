cb61999205dc85d1c16b453005f853abf1bc1cc3
Mike Thai <mta24@sfu.ca>
2019-07-11 14:49:36 -0700

    fix course info view

--- app/controllers/course_infos_controller.rb
+++ app/controllers/course_infos_controller.rb
@@ -1,4 +1,24 @@
 class CourseInfosController < ApplicationController
   def index
+    @course_infos = CourseInfo.search(params[:search]).all.order(:department)
+  end
+
+  def show
+    @course_infos = CourseInfo.find(params[:id])
+  end
+
+  def new
+  end
+
+  # def create
+  #   @course = Course.new(course_info_params)
+  #
+  #   @course.save
+  #   redirect_to @course
+  # end
+
+  private
+  def course_info_params
+    params.require(:course).permit(:year, :semester, :department, :coursenumber, :section, :info_description, :info_prerequisites, :info_corequisites)
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -9,4 +9,15 @@ class Search < ApplicationRecord
     return courses
 
   end
+
+  def search_course_infos
+    course_infos = CourseInfo.all
+    course_infos = course_infos.where(["year ILIKE ?", "%#{year}%"]) if year.present?
+    course_infos = course_infos.where(["semester ILIKE ?", "%#{semester}%"]) if semester.present?
+    course_infos = course_infos.where(["subject ILIKE ?", "%#{department}%"]) if department.present?
+    course_infos = course_infos.where(["cast(number as text) ILIKE ?", "%#{coursenumber}%"]) if coursenumber.present?
+
+    return course_infos
+
+  end
 end
