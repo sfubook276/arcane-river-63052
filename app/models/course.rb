d12b007d2931301be7dc61558ab1d14d0b806e4d
Mike Thai <mta24@sfu.ca>
2019-07-14 17:47:07 -0700

    simple search fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   # params :subject, :number, :credits, :prerequisites, :corequisites
   def self.search(search)
     if search
-      where(["cast(subject as text) ILIKE ? or cast(number as text) ILIKE ?", "%#{search}%", "%#{search}%"])
+      where(["cast(department as text) ILIKE ? or cast(coursenumber as text) ILIKE ?", "%#{search}%", "%#{search}%"])
       # LIKE for case-sensitive, ILIKE for case-insensitive
     else
       all
