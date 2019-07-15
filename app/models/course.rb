ea9896da796ff8d224481e507400b1e7a34b7ca7
Mike Thai <mta24@sfu.ca>
2019-07-14 22:34:07 -0700

    simple search sort by dept, coursenumber

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,10 +2,10 @@ class Course < ApplicationRecord
   # params :subject, :number, :credits, :prerequisites, :corequisites
   def self.search(search)
     if search
-      where(["cast(department as text) ILIKE ? or cast(coursenumber as text) ILIKE ?", "%#{search}%", "%#{search}%"])
+      where(["cast(department as text) ILIKE ? or cast(coursenumber as text) ILIKE ?", "%#{search}%", "%#{search}%"]).order(:department, :coursenumber)
       # LIKE for case-sensitive, ILIKE for case-insensitive
     else
-      all
+      none
     end
   end
 end
