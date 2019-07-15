94b32c3522270e3f67b6c08cde3f1b7234a50b34
Mike Thai <mta24@sfu.ca>
2019-07-14 22:14:19 -0700

    searches sorted by dept, coursenumber

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,11 +12,11 @@ class Search < ApplicationRecord
 
   def search_course_infos
     course_infos = CourseInfo.all
-    course_infos = course_infos.where(["year ILIKE ?", "%#{year}%"]) if year.present?
-    course_infos = course_infos.where(["semester ILIKE ?", "%#{semester}%"]) if semester.present?
-    course_infos = course_infos.where(["department ILIKE ?", "%#{department}%"]) if department.present?
-    course_infos = course_infos.where(["cast(coursenumber as text) ILIKE ?", "%#{coursenumber}%"]) if coursenumber.present?
-    course_infos = course_infos.where(["cast(info_prerequisites as text) ILIKE ?", "%#{info_prerequisites}%"]) if info_prerequisites.present?
+    course_infos = course_infos.where(["year ILIKE ?", "%#{year}%"]).order(:department, :coursenumber) if year.present?
+    course_infos = course_infos.where(["semester ILIKE ?", "%#{semester}%"]).order(:department, :coursenumber) if semester.present?
+    course_infos = course_infos.where(["department ILIKE ?", "%#{department}%"]).order(:department, :coursenumber) if department.present?
+    course_infos = course_infos.where(["cast(coursenumber as text) ILIKE ?", "%#{coursenumber}%"]).order(:department, :coursenumber) if coursenumber.present?
+    course_infos = course_infos.where(["cast(info_prerequisites as text) ILIKE ?", "%#{info_prerequisites}%"]).order(:department, :coursenumber) if info_prerequisites.present?
 
     return course_infos
 
